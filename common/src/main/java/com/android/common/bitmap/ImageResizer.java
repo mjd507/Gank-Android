01e39c5ec702576cd43cebe5d69cd68a1ece5a2a
majiandong <836240219@qq.com>
2016-12-20 19:45:27 +0800

    Update

--- common/src/main/java/com/android/common/bitmap/ImageResizer.java
+++ common/src/main/java/com/android/common/bitmap/ImageResizer.java
@@ -4,50 +4,80 @@ import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 
+import java.io.FileDescriptor;
+
 /**
- * 描述: 设定图片大小
+ * 描述: 图片压缩
  * Created by mjd on 2016/12/12.
  */
 
 public class ImageResizer {
 
 
-    /**
-     * 根据指定大小等比例压缩图片
-     */
-    public Bitmap decodeSampledBitmapFromResource(Resources res, int resId, int reqWidth, int reqHeight) {
+    public static Bitmap decodeSampledBitmapFromResource(Resources res, int resId, int reqWidth, int reqHeight) {
 
         BitmapFactory.Options options = new BitmapFactory.Options();
         options.inJustDecodeBounds = true;
-        //不加载图片,只检查图片大小
         BitmapFactory.decodeResource(res, resId, options);
 
         options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
 
-        //加载压缩后的图片
         options.inJustDecodeBounds = false;
         return BitmapFactory.decodeResource(res, resId, options);
 
     }
 
-    private int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+
+    public static Bitmap decodeSampledBitmapFromDescriptor(FileDescriptor fileDescriptor, int reqWidth, int reqHeight) {
+
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFileDescriptor(fileDescriptor, null, options);
+
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+        options.inJustDecodeBounds = false;
+
+        return BitmapFactory.decodeFileDescriptor(fileDescriptor, null, options);
+    }
+
+
+    public static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // BEGIN_INCLUDE (calculate_sample_size)
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
         int inSampleSize = 1;
 
-        if (reqWidth == 0 || reqHeight == 0) {
-            return inSampleSize;
-        }
+        if (height > reqHeight || width > reqWidth) {
 
-        int w = options.outWidth;
-        int h = options.outHeight;
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
 
-        if (w > reqWidth || h > reqHeight) {
-            w = w / 2;
-            h = h / 2;
-            while (w >= reqWidth && h >= reqHeight) {
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) > reqHeight && (halfWidth / inSampleSize) > reqWidth) {
                 inSampleSize *= 2;
+            }
+
+            // This offers some additional logic in case the image has a strange
+            // aspect ratio. For example, a panorama may have a much larger
+            // width than height. In these cases the total pixels might still
+            // end up being too large to fit comfortably in memory, so we should
+            // be more aggressive with sample down the image (=larger inSampleSize).
+
+            long totalPixels = width * height / inSampleSize;
+
+            // Anything more than 2x the requested pixels we'll sample down further
+            final long totalReqPixelsCap = reqWidth * reqHeight * 2;
+
+            while (totalPixels > totalReqPixelsCap) {
+                inSampleSize *= 2;
+                totalPixels /= 2;
             }
         }
         return inSampleSize;
+        // END_INCLUDE (calculate_sample_size)
     }
 
 }
---
+++ common/src/test/java/com/cleaner/test/DiskLrucacheTest.java
@@ -0,0 +1,82 @@
+package com.cleaner.test;
+
+import android.graphics.Bitmap;
+
+import com.android.common.bitmap.ImageResizer;
+import com.android.common.bitmap.cache.DiskLruCache;
+import com.android.common.utils.CloseUtils;
+import com.android.common.utils.MD5Utils;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileDescriptor;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * 描述:
+ * Created by mjd on 2016/12/20.
+ */
+
+public class DiskLrucacheTest {
+    private DiskLruCache mDiskLrucache;
+    String url = "picurl";
+
+
+    public void test() throws IOException {
+        String key = MD5Utils.getMd5Str(url.getBytes());
+        DiskLruCache.Editor editor = mDiskLrucache.edit(key);
+        if (editor != null) {
+            OutputStream outputStream = editor.newOutputStream(0);
+            if (downloadUrlToStream(url, outputStream)) {
+                editor.commit();
+            } else {
+                editor.abort();
+            }
+            mDiskLrucache.flush();
+        }
+    }
+
+    private boolean downloadUrlToStream(String url, OutputStream outputStream) {
+        URL urlStr = null;
+        HttpURLConnection conn = null;
+        BufferedOutputStream out = null;
+        BufferedInputStream in = null;
+        try {
+            urlStr = new URL(url);
+            conn = (HttpURLConnection) urlStr.openConnection();
+            out = new BufferedOutputStream(outputStream);
+            in = new BufferedInputStream(conn.getInputStream());
+            int b;
+            while ((b = in.read()) != -1) {
+                out.write(b);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (conn != null) {
+                conn.disconnect();
+            }
+            CloseUtils.closeIO(in, out);
+        }
+        return false;
+    }
+
+    private Bitmap get(String url,int reqWidth,int reqHeight) throws IOException {
+        Bitmap bitmap = null;
+        String key = MD5Utils.getMd5Str(url.getBytes());
+        DiskLruCache.Snapshot snapshot = mDiskLrucache.get(key);
+        if(snapshot!=null){
+            FileInputStream fis = (FileInputStream) snapshot.getInputStream(0);
+            FileDescriptor fileDescriptor = fis.getFD();
+            bitmap = ImageResizer.decodeSampledBitmapFromDescriptor(fileDescriptor, reqWidth, reqHeight);
+            //add to memory
+        }
+        return bitmap;
+    }
+
+
+}
