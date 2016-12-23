3a998319e4ac4e03dd68d82b5bc9c90b5ec2c134
majiandong <836240219@qq.com>
2016-12-23 21:14:44 +0800

    update

--- common/src/main/java/com/android/common/utils/TimeUtils.java
+++ common/src/main/java/com/android/common/utils/TimeUtils.java
@@ -2,7 +2,6 @@ package com.android.common.utils;
 
 import android.annotation.SuppressLint;
 
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -18,11 +17,10 @@ public class TimeUtils {
     private static final String DEFAULT_PATTERN = "yyyy-MM-dd HH:mm:ss";
 
 
-    //---------------- millis <=> String  millis <==> date  String <=> date  start  -------------
+    //---------------- millis => String  millis <==> date  date => String  start  -------------
 
     /**
      * 将时间戳转为时间字符串
-     * <p>格式为yyyy-MM-dd HH:mm:ss</p>
      */
     public static String millis2String(long millis) {
         return new SimpleDateFormat(DEFAULT_PATTERN, Locale.getDefault()).format(new Date(millis));
@@ -30,52 +28,13 @@ public class TimeUtils {
 
     /**
      * 将时间戳转为时间字符串
-     * <p>格式为pattern</p>
      */
     public static String millis2String(long millis, String pattern) {
         return new SimpleDateFormat(pattern, Locale.getDefault()).format(new Date(millis));
     }
 
     /**
-     * 将时间字符串转为时间戳
-     * <p>time格式为yyyy-MM-dd HH:mm:ss</p>
-     */
-    public static long string2Millis(String time) {
-        return string2Millis(time, DEFAULT_PATTERN);
-    }
-
-    /**
-     * 将时间字符串转为时间戳
-     * <p>time格式为pattern</p>
-     */
-    public static long string2Millis(String time, String pattern) {
-        try {
-            return new SimpleDateFormat(pattern, Locale.getDefault()).parse(time).getTime();
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        return -1;
-    }
-
-    /**
-     * 将时间字符串转为Date类型
-     * <p>time格式为yyyy-MM-dd HH:mm:ss</p>
-     */
-    public static Date string2Date(String time) {
-        return string2Date(time, DEFAULT_PATTERN);
-    }
-
-    /**
-     * 将时间字符串转为Date类型
-     * <p>time格式为pattern</p>
-     */
-    public static Date string2Date(String time, String pattern) {
-        return new Date(string2Millis(time, pattern));
-    }
-
-    /**
      * 将Date类型转为时间字符串
-     * <p>格式为yyyy-MM-dd HH:mm:ss</p>
      */
     public static String date2String(Date date) {
         return date2String(date, DEFAULT_PATTERN);
@@ -83,7 +42,6 @@ public class TimeUtils {
 
     /**
      * 将Date类型转为时间字符串
-     * <p>格式为pattern</p>
      */
     public static String date2String(Date date, String pattern) {
         return new SimpleDateFormat(pattern, Locale.getDefault()).format(date);
@@ -103,11 +61,11 @@ public class TimeUtils {
         return new Date(millis);
     }
 
-    //---------------- millis <=> String  millis <==> date  String <=> date  end  -------------
-
 
     //---------------- 获取合适型两个时间差 返回 String 类型的 start ----------------
+
     /**
+     * 获取合适型两个时间差
      * precision = 0，返回null
      * precision = 1，返回天
      * precision = 2，返回天和小时
@@ -115,39 +73,8 @@ public class TimeUtils {
      * precision = 4，返回天、小时、分钟和秒
      * precision >= 5，返回天、小时、分钟、秒和毫秒
      */
-
-    /**
-     * 获取合适型两个时间差
-     * <p>time0和time1格式都为yyyy-MM-dd HH:mm:ss</p>
-     */
-    public static String getFitTimeSpan(String time0, String time1, int precision) {
-        return millis2FitTimeSpan(Math.abs(string2Millis(time0, DEFAULT_PATTERN) - string2Millis(time1, DEFAULT_PATTERN)), precision);
-    }
-
-    /**
-     * 获取合适型两个时间差
-     * <p>time0和time1格式都为pattern</p>
-     */
-    public static String getFitTimeSpan(String time0, String time1, int precision, String pattern) {
-        return millis2FitTimeSpan(Math.abs(string2Millis(time0, pattern) - string2Millis(time1, pattern)), precision);
-    }
-
-    /**
-     * 获取合适型两个时间差
-     */
-    public static String getFitTimeSpan(Date date0, Date date1, int precision) {
-        return millis2FitTimeSpan(Math.abs(date2Millis(date0) - date2Millis(date1)), precision);
-    }
-
-    /**
-     * 获取合适型两个时间差
-     */
     public static String getFitTimeSpan(long millis0, long millis1, int precision) {
-        return millis2FitTimeSpan(Math.abs(millis0 - millis1), precision);
-    }
-
-    @SuppressLint("DefaultLocale")
-    public static String millis2FitTimeSpan(long millis, int precision) {
+        long millis = Math.abs(millis0 - millis1);
         if (millis <= 0 || precision <= 0) return null;
         StringBuilder sb = new StringBuilder();
         String[] units = {"天", "小时", "分钟", "秒", "毫秒"};
@@ -163,79 +90,9 @@ public class TimeUtils {
         return sb.toString();
     }
 
-    //---------------- 获取合适型两个时间差 返回 String 类型的 end ----------------
-
-
-    //---------------- 获取当前时间 start ----------------
     /**
-     * 获取当前毫秒时间戳
-     */
-    public static long getNowTimeMills() {
-        return System.currentTimeMillis();
-    }
-
-    /**
-     * 获取当前时间字符串
-     */
-    public static String getNowTimeString() {
-        return millis2String(System.currentTimeMillis(), DEFAULT_PATTERN);
-    }
-
-    /**
-     * 获取当前时间字符串
-     * <p>格式为pattern</p>
-     */
-    public static String getNowTimeString(String pattern) {
-        return millis2String(System.currentTimeMillis(), pattern);
-    }
-
-    /**
-     * 获取当前Date
-     */
-    public static Date getNowTimeDate() {
-        return new Date();
-    }
-
-    //---------------- 获取当前时间 end ----------------
-
-
-    //---------------- 获取合适型与当前时间的差 start ----------------
-
-    /**
-     * 获取合适型与当前时间的差
-     */
-    public static String getFitTimeSpanByNow(String time, int precision) {
-        return getFitTimeSpan(getNowTimeString(), time, precision, DEFAULT_PATTERN);
-    }
-
-    /**
-     * 获取合适型与当前时间的差
-     * <p>time格式为pattern</p>
-     */
-    public static String getFitTimeSpanByNow(String time, int precision, String pattern) {
-        return getFitTimeSpan(getNowTimeString(), time, precision, pattern);
-    }
-
-    /**
-     * 获取合适型与当前时间的差
-     */
-    public static String getFitTimeSpanByNow(Date date, int precision) {
-        return getFitTimeSpan(getNowTimeDate(), date, precision);
-    }
-
-    /**
-     * 获取合适型与当前时间的差
-     */
-    public static String getFitTimeSpanByNow(long millis, int precision) {
-        return getFitTimeSpan(System.currentTimeMillis(), millis, precision);
-    }
-
-    //---------------- 获取合适型与当前时间的差 end ----------------
-
-
-
-    //---------------- 获取友好型与当前时间的差 start ----------------
-    /**
+     * 获取友好型与当前时间的差
+     * <p>
      * 如果小于1秒钟内，显示刚刚
      * 如果在1分钟内，显示XXX秒前
      * 如果在1小时内，显示XXX分钟前
@@ -243,32 +100,6 @@ public class TimeUtils {
      * 如果是昨天的，显示昨天15:32
      * 其余显示，2016-10-15
      * 时间不合法的情况全部日期和时间信息，如星期六 十月 27 14:21:20 CST 2007
-     */
-
-    /**
-     * 获取友好型与当前时间的差
-     */
-    public static String getFriendlyTimeSpanByNow(String time) {
-        return getFriendlyTimeSpanByNow(time, DEFAULT_PATTERN);
-    }
-
-    /**
-     * 获取友好型与当前时间的差
-     * <p>time格式为pattern</p>
-     */
-    public static String getFriendlyTimeSpanByNow(String time, String pattern) {
-        return getFriendlyTimeSpanByNow(string2Millis(time, pattern));
-    }
-
-    /**
-     * 获取友好型与当前时间的差
-     */
-    public static String getFriendlyTimeSpanByNow(Date date) {
-        return getFriendlyTimeSpanByNow(date.getTime());
-    }
-
-    /**
-     * 获取友好型与当前时间的差
      */
     @SuppressLint("DefaultLocale")
     public static String getFriendlyTimeSpanByNow(long millis) {
@@ -294,35 +125,6 @@ public class TimeUtils {
         }
     }
 
-    //---------------- 获取友好型与当前时间的差 end ----------------
-
-
-    /**
-     * 判断是否同一天
-     * <p>time格式为yyyy-MM-dd HH:mm:ss</p>
-     *
-     * @param time 时间字符串
-     * @return {@code true}: 是<br>{@code false}: 否
-     */
-    public static boolean isSameDay(String time) {
-        return isSameDay(string2Millis(time, DEFAULT_PATTERN));
-    }
-
-    /**
-     * 判断是否同一天
-     * <p>time格式为pattern</p>
-     */
-    public static boolean isSameDay(String time, String pattern) {
-        return isSameDay(string2Millis(time, pattern));
-    }
-
-    /**
-     * 判断是否同一天
-     */
-    public static boolean isSameDay(Date date) {
-        return isSameDay(date.getTime());
-    }
-
     /**
      * 判断是否同一天
      */
@@ -333,342 +135,84 @@ public class TimeUtils {
 
     /**
      * 判断是否闰年
-     * <p>time格式为yyyy-MM-dd HH:mm:ss</p>
-     *
-     * @param time 时间字符串
-     * @return {@code true}: 闰年<br>{@code false}: 平年
-     */
-    public static boolean isLeapYear(String time) {
-        return isLeapYear(string2Date(time, DEFAULT_PATTERN));
-    }
-
-    /**
-     * 判断是否闰年
-     * <p>time格式为pattern</p>
-     *
-     * @param time    时间字符串
-     * @param pattern 时间格式
-     * @return {@code true}: 闰年<br>{@code false}: 平年
-     */
-    public static boolean isLeapYear(String time, String pattern) {
-        return isLeapYear(string2Date(time, pattern));
-    }
-
-    /**
-     * 判断是否闰年
-     *
-     * @param date Date类型时间
-     * @return {@code true}: 闰年<br>{@code false}: 平年
-     */
-    public static boolean isLeapYear(Date date) {
-        Calendar cal = Calendar.getInstance();
-        cal.setTime(date);
-        int year = cal.get(Calendar.YEAR);
-        return isLeapYear(year);
-    }
-
-    /**
-     * 判断是否闰年
-     *
-     * @param millis 毫秒时间戳
-     * @return {@code true}: 闰年<br>{@code false}: 平年
      */
     public static boolean isLeapYear(long millis) {
-        return isLeapYear(millis2Date(millis));
-    }
-
-    /**
-     * 判断是否闰年
-     *
-     * @param year 年份
-     * @return {@code true}: 闰年<br>{@code false}: 平年
-     */
-    public static boolean isLeapYear(int year) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(millis2Date(millis));
+        int year = cal.get(Calendar.YEAR);
         return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
     }
 
-    /**
-     * 获取星期
-     * <p>time格式为yyyy-MM-dd HH:mm:ss</p>
-     *
-     * @param time 时间字符串
-     * @return 星期
-     */
-    public static String getWeek(String time) {
-        return getWeek(string2Date(time, DEFAULT_PATTERN));
-    }
 
     /**
      * 获取星期
-     * <p>time格式为pattern</p>
-     *
-     * @param time    时间字符串
-     * @param pattern 时间格式
-     * @return 星期
-     */
-    public static String getWeek(String time, String pattern) {
-        return getWeek(string2Date(time, pattern));
-    }
-
-    /**
-     * 获取星期
-     *
-     * @param date Date类型时间
-     * @return 星期
-     */
-    public static String getWeek(Date date) {
-        return new SimpleDateFormat("EEEE", Locale.getDefault()).format(date);
-    }
-
-    /**
-     * 获取星期
-     *
-     * @param millis 毫秒时间戳
-     * @return 星期
      */
     public static String getWeek(long millis) {
-        return getWeek(new Date(millis));
+        return new SimpleDateFormat("EEEE", Locale.getDefault()).format(new Date(millis));
     }
 
     /**
-     * 获取星期
-     * <p>注意：周日的Index才是1，周六为7</p>
-     * <p>time格式为yyyy-MM-dd HH:mm:ss</p>
-     *
-     * @param time 时间字符串
-     * @return 1...5
+     * 获取星期 index
+     * 注意: 周日的Index才是1，周六为7
      */
-    public static int getWeekIndex(String time) {
-        return getWeekIndex(string2Date(time, DEFAULT_PATTERN));
-    }
-
-    /**
-     * 获取星期
-     * <p>注意：周日的Index才是1，周六为7</p>
-     * <p>time格式为pattern</p>
-     *
-     * @param time    时间字符串
-     * @param pattern 时间格式
-     * @return 1...7
-     */
-    public static int getWeekIndex(String time, String pattern) {
-        return getWeekIndex(string2Date(time, pattern));
-    }
-
-    /**
-     * 获取星期
-     * <p>注意：周日的Index才是1，周六为7</p>
-     *
-     * @param date Date类型时间
-     * @return 1...7
-     */
-    public static int getWeekIndex(Date date) {
+    public static int getWeekIndex(long millis) {
         Calendar cal = Calendar.getInstance();
-        cal.setTime(date);
+        cal.setTimeInMillis(millis);
         return cal.get(Calendar.DAY_OF_WEEK);
     }
 
-    /**
-     * 获取星期
-     * <p>注意：周日的Index才是1，周六为7</p>
-     *
-     * @param millis 毫秒时间戳
-     * @return 1...7
-     */
-    public static int getWeekIndex(long millis) {
-        return getWeekIndex(millis2Date(millis));
-    }
 
     /**
      * 获取月份中的第几周
-     * <p>注意：国外周日才是新的一周的开始</p>
-     * <p>time格式为yyyy-MM-dd HH:mm:ss</p>
+     * 注意：国外周日才是新的一周的开始
      *
-     * @param time 时间字符串
      * @return 1...5
      */
-    public static int getWeekOfMonth(String time) {
-        return getWeekOfMonth(string2Date(time, DEFAULT_PATTERN));
-    }
-
-    /**
-     * 获取月份中的第几周
-     * <p>注意：国外周日才是新的一周的开始</p>
-     * <p>time格式为pattern</p>
-     *
-     * @param time    时间字符串
-     * @param pattern 时间格式
-     * @return 1...5
-     */
-    public static int getWeekOfMonth(String time, String pattern) {
-        return getWeekOfMonth(string2Date(time, pattern));
-    }
-
-    /**
-     * 获取月份中的第几周
-     * <p>注意：国外周日才是新的一周的开始</p>
-     *
-     * @param date Date类型时间
-     * @return 1...5
-     */
-    public static int getWeekOfMonth(Date date) {
+    public static int getWeekOfMonth(long millis) {
         Calendar cal = Calendar.getInstance();
-        cal.setTime(date);
+        cal.setTimeInMillis(millis);
         return cal.get(Calendar.WEEK_OF_MONTH);
     }
 
     /**
-     * 获取月份中的第几周
-     * <p>注意：国外周日才是新的一周的开始</p>
-     *
-     * @param millis 毫秒时间戳
-     * @return 1...5
-     */
-    public static int getWeekOfMonth(long millis) {
-        return getWeekOfMonth(millis2Date(millis));
-    }
-
-    /**
      * 获取年份中的第几周
      * <p>注意：国外周日才是新的一周的开始</p>
-     * <p>time格式为yyyy-MM-dd HH:mm:ss</p>
-     *
-     * @param time 时间字符串
-     * @return 1...54
      */
-    public static int getWeekOfYear(String time) {
-        return getWeekOfYear(string2Date(time, DEFAULT_PATTERN));
-    }
-
-    /**
-     * 获取年份中的第几周
-     * <p>注意：国外周日才是新的一周的开始</p>
-     * <p>time格式为pattern</p>
-     *
-     * @param time    时间字符串
-     * @param pattern 时间格式
-     * @return 1...54
-     */
-    public static int getWeekOfYear(String time, String pattern) {
-        return getWeekOfYear(string2Date(time, pattern));
-    }
-
-    /**
-     * 获取年份中的第几周
-     * <p>注意：国外周日才是新的一周的开始</p>
-     *
-     * @param date Date类型时间
-     * @return 1...54
-     */
-    public static int getWeekOfYear(Date date) {
+    public static int getWeekOfYear(long millis) {
         Calendar cal = Calendar.getInstance();
-        cal.setTime(date);
+        cal.setTimeInMillis(millis);
         return cal.get(Calendar.WEEK_OF_YEAR);
     }
 
-    /**
-     * 获取年份中的第几周
-     * <p>注意：国外周日才是新的一周的开始</p>
-     *
-     * @param millis 毫秒时间戳
-     * @return 1...54
-     */
-    public static int getWeekOfYear(long millis) {
-        return getWeekOfYear(millis2Date(millis));
-    }
 
     private static final String[] CHINESE_ZODIAC = {"猴", "鸡", "狗", "猪", "鼠", "牛", "虎", "兔", "龙", "蛇", "马", "羊"};
 
     /**
      * 获取生肖
-     * <p>time格式为yyyy-MM-dd HH:mm:ss</p>
-     *
-     * @param time 时间字符串
-     * @return 生肖
      */
-    public static String getChineseZodiac(String time) {
-        return getChineseZodiac(string2Date(time, DEFAULT_PATTERN));
-    }
-
-    /**
-     * 获取生肖
-     * <p>time格式为pattern</p>
-     *
-     * @param time    时间字符串
-     * @param pattern 时间格式
-     * @return 生肖
-     */
-    public static String getChineseZodiac(String time, String pattern) {
-        return getChineseZodiac(string2Date(time, pattern));
-    }
-
-    /**
-     * 获取生肖
-     *
-     * @param date Date类型时间
-     * @return 生肖
-     */
-    public static String getChineseZodiac(Date date) {
+    public static String getChineseZodiac(long millis) {
         Calendar cal = Calendar.getInstance();
-        cal.setTime(date);
+        cal.setTimeInMillis(millis);
         return CHINESE_ZODIAC[cal.get(Calendar.YEAR) % 12];
     }
 
     /**
      * 获取生肖
-     *
-     * @param millis 毫秒时间戳
-     * @return 生肖
-     */
-    public static String getChineseZodiac(long millis) {
-        return getChineseZodiac(millis2Date(millis));
-    }
-
-    /**
-     * 获取生肖
-     *
-     * @param year 年
-     * @return 生肖
      */
     public static String getChineseZodiac(int year) {
         return CHINESE_ZODIAC[year % 12];
     }
+
 
     private static final String[] ZODIAC = {"水瓶座", "双鱼座", "白羊座", "金牛座", "双子座", "巨蟹座", "狮子座", "处女座", "天秤座", "天蝎座", "射手座", "魔羯座"};
     private static final int[] ZODIAC_FLAGS = {20, 19, 21, 21, 21, 22, 23, 23, 23, 24, 23, 22};
 
     /**
      * 获取星座
-     * <p>time格式为yyyy-MM-dd HH:mm:ss</p>
-     *
-     * @param time 时间字符串
-     * @return 生肖
      */
-    public static String getZodiac(String time) {
-        return getZodiac(string2Date(time, DEFAULT_PATTERN));
-    }
-
-    /**
-     * 获取星座
-     * <p>time格式为pattern</p>
-     *
-     * @param time    时间字符串
-     * @param pattern 时间格式
-     * @return 生肖
-     */
-    public static String getZodiac(String time, String pattern) {
-        return getZodiac(string2Date(time, pattern));
-    }
-
-    /**
-     * 获取星座
-     *
-     * @param date Date类型时间
-     * @return 星座
-     */
-    public static String getZodiac(Date date) {
+    public static String getZodiac(long millis) {
         Calendar cal = Calendar.getInstance();
-        cal.setTime(date);
+        cal.setTimeInMillis(millis);
         int month = cal.get(Calendar.MONTH) + 1;
         int day = cal.get(Calendar.DAY_OF_MONTH);
         return getZodiac(month, day);
@@ -676,20 +220,6 @@ public class TimeUtils {
 
     /**
      * 获取星座
-     *
-     * @param millis 毫秒时间戳
-     * @return 星座
-     */
-    public static String getZodiac(long millis) {
-        return getZodiac(millis2Date(millis));
-    }
-
-    /**
-     * 获取星座
-     *
-     * @param month 月
-     * @param day   日
-     * @return 星座
      */
     public static String getZodiac(int month, int day) {
         return ZODIAC[day >= ZODIAC_FLAGS[month - 1]
