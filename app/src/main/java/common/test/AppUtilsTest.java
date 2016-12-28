1dcf48bb799f78fd0276b13d144bc150c670ad7c
majiandong <836240219@qq.com>
2016-12-28 15:57:02 +0800

    Update

---
+++ app/src/main/java/common/test/AppUtilsTest.java
@@ -0,0 +1,92 @@
+package common.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import java.io.File;
+
+import common.utils.AppUtils;
+import common.utils.SDCardUtils;
+import common.utils.ToastUtils;
+
+/**
+ * 描述:
+ * Created by mjd on 2016/12/28.
+ */
+
+public class AppUtilsTest extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        LinearLayout contentView = new LinearLayout(this);
+        contentView.setOrientation(LinearLayout.VERTICAL);
+
+        Button btnInstallApp = new Button(this);
+        btnInstallApp.setText("安装下载好的 app");
+        btnInstallApp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AppUtils.installApp(getApplicationContext(), new File(SDCardUtils.getSDCardPath() + "SDCardUtilsTest.apk"));
+            }
+        });
+
+
+        Button btnAppInfo = new Button(this);
+        btnAppInfo.setText("app 版本号 和 版本码");
+        btnAppInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String appVersionName = AppUtils.getAppVersionName(getApplicationContext());
+                int appVersionCode = AppUtils.getAppVersionCode(getApplicationContext());
+                ToastUtils.showShort(getApplicationContext(), "版本号:" + appVersionName + "  版本码:" + appVersionCode);
+            }
+        });
+
+        Button btnAppForeground = new Button(this);
+        btnAppForeground.setText("app 是否在前台");
+        btnAppForeground.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean appForeground = AppUtils.isAppForeground(getApplicationContext());
+                ToastUtils.showShort(getApplicationContext(), appForeground ? "app在前台" : "app进入后台");
+            }
+        });
+
+
+        Button btnAppClean = new Button(this);
+        btnAppClean.setText("清除 app 数据");
+        btnAppClean.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean b = AppUtils.cleanAppData(getApplicationContext(), new File[]{});
+                ToastUtils.showShort(getApplicationContext(), b ? "清除成功" : "清除失败");
+            }
+        });
+
+        contentView.addView(btnInstallApp);
+        contentView.addView(btnAppInfo);
+        contentView.addView(btnAppForeground);
+        contentView.addView(btnAppClean);
+
+
+        setContentView(contentView);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        boolean appForeground = AppUtils.isAppForeground(getApplicationContext());
+        ToastUtils.showShort(getApplicationContext(), appForeground ? "app在前台" : "app进入后台");
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        boolean appForeground = AppUtils.isAppForeground(getApplicationContext());
+        ToastUtils.showShort(getApplicationContext(), appForeground ? "app在前台" : "app进入后台");
+    }
+}
--- app/src/main/java/common/utils/AppUtils.java
+++ app/src/main/java/common/utils/AppUtils.java
@@ -1,10 +1,105 @@
 package common.utils;
 
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.webkit.MimeTypeMap;
+
+import java.io.File;
+import java.util.List;
+
 /**
  * 描述:
  * Created by mjd on 2016/12/28.
  */
 
 public class AppUtils {
-    
+
+    /**
+     * 安装App（支持6.0）
+     */
+    public static void installApp(Context context, File file) {
+        if (file != null && file.exists()) {
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            String type;
+            if (Build.VERSION.SDK_INT < 23) {
+                type = "application/vnd.android.package-archive";
+            } else {
+                String filePath = file.getPath();
+                String extension = "";
+                int lastPoi = filePath.lastIndexOf('.');
+                int lastSep = filePath.lastIndexOf(File.separator);
+                if (lastPoi != -1 && lastSep >= lastPoi) {
+                    extension = filePath.substring(lastPoi + 1);
+                }
+                type = MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension);
+            }
+            intent.setDataAndType(Uri.fromFile(file), type);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(intent);
+        }
+    }
+
+    /**
+     * 获取App版本号
+     */
+    public static String getAppVersionName(Context context) {
+        try {
+            PackageManager pm = context.getPackageManager();
+            PackageInfo pi = pm.getPackageInfo(context.getPackageName(), 0);
+            return pi == null ? null : pi.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * 获取App版本码
+     */
+    public static int getAppVersionCode(Context context) {
+        try {
+            PackageManager pm = context.getPackageManager();
+            PackageInfo pi = pm.getPackageInfo(context.getPackageName(), 0);
+            return pi == null ? -1 : pi.versionCode;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+            return -1;
+        }
+    }
+
+    /**
+     * 判断App是否处于前台
+     */
+    public static boolean isAppForeground(Context context) {
+        ActivityManager manager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningAppProcessInfo> infos = manager.getRunningAppProcesses();
+        if (infos == null || infos.size() == 0) return false;
+        for (ActivityManager.RunningAppProcessInfo info : infos) {
+            if (info.importance == ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND) {
+                return info.processName.equals(context.getPackageName());
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 清除App所有数据
+     * @param dirs 自定义的目录 (不指定默认会清除 应用内部的 File,DB,SP,Cache,ExternalCache)
+     */
+    public static boolean cleanAppData(Context context, File... dirs) {
+        boolean isSuccess = CleanUtils.cleanInternalCache(context);
+        isSuccess &= CleanUtils.cleanInternalDbs(context);
+        isSuccess &= CleanUtils.cleanInternalSP(context);
+        isSuccess &= CleanUtils.cleanInternalFiles(context);
+        isSuccess &= CleanUtils.cleanExternalCache(context);
+        for (File dir : dirs) {
+            isSuccess &= CleanUtils.cleanCustomCache(dir);
+        }
+        return isSuccess;
+    }
 }
