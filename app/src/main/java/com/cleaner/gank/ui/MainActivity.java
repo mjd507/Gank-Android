4e73b73433c0448b2154a06000c46a85b556d863
majiandong <836240219@qq.com>
2017-02-10 22:55:15 +0800

    update

---
+++ app/src/main/java/com/cleaner/gank/ui/MainActivity.java
@@ -0,0 +1,22 @@
+package com.cleaner.gank.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.cleaner.commonandroid.R;
+
+import common.ui.BaseActivity;
+
+/**
+ * 描述:
+ * Created by mjd on 2017/2/10.
+ */
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
